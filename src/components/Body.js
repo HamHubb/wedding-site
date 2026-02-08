122dca4c6e72d0c7965669324d4ea7e740c2052f
Hairo Morales <moraleshairo@gmail.com>
2026-02-07 21:41:35 -0800

    Added a break to Home

--- src/components/Body.js
+++ src/components/Body.js
@@ -91,7 +91,7 @@ const Body = () => {
 
 
             <p>
-              If you're in town for the weekend join us for drinks Friday, April 24, 2026 @7:30pm! <br></br>Casual Cocktail Attire.
+              If you're in town for the weekend join us for drinks<br></br> Friday, April 24, 2026 @7:30pm! <br></br>Casual Cocktail Attire.
             </p>
             <p>Please join us for welcome drinks at the Tipsy Goat - Front Patio </p>
             <p></p>
